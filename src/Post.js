2085c8b768938a89612453d6afa1d2bbdb55533a
George Garman <georgecolingarman.uk@gmail.com>
2022-09-18 18:17:22 +0100

    post functionality works

--- src/Post.js
+++ src/Post.js
@@ -12,21 +12,21 @@ import { ArrowBackIcon } from "@chakra-ui/icons"
 import { useParams, useNavigate } from "react-router-dom"
 import axios from "axios"
 import { useEffect, useState } from "react"
+import postService from "./postService"
+
+const apiToken =
+  "dd220514010f795c36fdddf911179841561645b2ca0bb160874f73bd70eedbbbe873f016a4134f6a771ff42fe2c1a99229a5c958fd45ab0b08622ca6ffb24541b33cbc53477e995343bd75aff56d5cf8abe6341c912b885fb3b112f7dc575f2bf93ff7c279cf68d6d778760b5a783e19064d139e392c17aba27f421e29c1ee3c"
 
 const baseUrl = "http://localhost:1337"
 
-function Post(props) {
+function Post() {
   let params = useParams()
   let navigate = useNavigate()
   let [post, setPost] = useState()
   useEffect(() => {
-    const configuration = {
-      method: "get",
-      url: "http://localhost:1337/api/posts/",
-    }
-    axios(configuration).then((result) => {
-      setPost(result.data)
-      console.log(result.data)
+    postService.getSingle(params.postId).then((result) => {
+      console.log("result :>> ", result)
+      setPost(result)
     })
   }, [])
   if (!post) return <></>
--- src/PostPreviews.js
+++ src/PostPreviews.js
@@ -3,28 +3,14 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { background, Box, flexbox, Heading, Text } from "@chakra-ui/react"
 import PostPreview from "./PostPreview"
-
-const apiToken =
-  "dd220514010f795c36fdddf911179841561645b2ca0bb160874f73bd70eedbbbe873f016a4134f6a771ff42fe2c1a99229a5c958fd45ab0b08622ca6ffb24541b33cbc53477e995343bd75aff56d5cf8abe6341c912b885fb3b112f7dc575f2bf93ff7c279cf68d6d778760b5a783e19064d139e392c17aba27f421e29c1ee3c"
-
-const baseUrl = "http://localhost:1337"
+import postService from "./postService"
 
 export default function PostPreviews() {
   let navigate = useNavigate()
   const [posts, setPosts] = useState([])
   useEffect(() => {
-    const configuration = {
-      method: "get",
-      url: "http://localhost:1337/api/posts?populate=%2A",
-      headers: {
-        authorization: `bearer ${apiToken}`,
-      },
-    }
-    axios(configuration).then((result) => {
-      const data = result.data.data
-      console.log("result :>> ", result)
-      console.log("data :>> ", data)
-      setPosts(data)
+    postService.getAll().then((result) => {
+      setPosts(result)
     })
   }, [])
 
